bf4a4f93e3a25a1cc0fd320516d9e836d591c954
jose manuel gonzales rivera <jgmanuelrivera@outlook.com.pe>
2019-01-09 01:49:28 -0500

    cambios en el servidor

--- servidor.js
+++ servidor.js
@@ -2,13 +2,7 @@ var express = require("express");
 var app     = express();
 var path    = require("path");
 
-
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-});
-
+app.use(express.static(__dirname + '/public'));
 app.listen(8080);
-
-
 
 console.log("Running at Port 8080");
