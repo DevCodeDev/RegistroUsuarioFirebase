f8381e40011a0f2ffaee03e67cafdc8d1c7f3c68
jose manuel gonzales rivera <jgmanuelrivera@outlook.com.pe>
2019-01-09 01:44:39 -0500

    server node con dirname

--- servidor.js
+++ servidor.js
@@ -1,22 +1,14 @@
-const express = require('express');
-const fs = require('fs');
-var http = require('http');
+var express = require("express");
+var app     = express();
+var path    = require("path");
 
-// function onRequest(request, response){
-//     console.log("Peticion recibida");
-//     response.writeHead(200, {'content-type': 'text/html'});
-//     response.write('Hola mundo!!');
-//     response.end();
-// }
-//var server = http.createServer(onRequest);
-http.createServer(function(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-  
-    var file = fs.createReadStream('index.html');
-    file.pipe(response);
-  
-  }).listen(8080);
 
-//server.listen(8080);
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
 
-console.log("Servidor funcionando en http://localhost:8080");
+app.listen(8080);
+
+
+
+console.log("Running at Port 8080");
