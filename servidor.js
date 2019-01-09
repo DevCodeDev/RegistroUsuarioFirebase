d15563e0b9137b66a19deea8ff9f09d9bfb547f5
jose manuel gonzales rivera <jgmanuelrivera@outlook.com.pe>
2019-01-09 01:43:08 -0500

    prueba de servidores

--- servidor.js
+++ servidor.js
@@ -1,12 +1,22 @@
+const express = require('express');
+const fs = require('fs');
 var http = require('http');
 
-function onRequest(request, response){
-    console.log("Peticion recibida");
-    response.writeHead(200, {'content-type': 'text/html'});
-    response.write('Hola mundo!!');
-    response.end();
-}
-var server = http.createServer(onRequest);
-server.listen(8080);
+// function onRequest(request, response){
+//     console.log("Peticion recibida");
+//     response.writeHead(200, {'content-type': 'text/html'});
+//     response.write('Hola mundo!!');
+//     response.end();
+// }
+//var server = http.createServer(onRequest);
+http.createServer(function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+  
+    var file = fs.createReadStream('index.html');
+    file.pipe(response);
+  
+  }).listen(8080);
+
+//server.listen(8080);
 
 console.log("Servidor funcionando en http://localhost:8080");
